2d003e8fcfb4509dc63e662a18c6865581efd361
trungngotdt <trungngotdt@outlook.com.vn>
2018-05-09 21:11:27 +0700

    commit 1

---
+++ Xamarin_Calculator/Xamarin_Calculator.Android/obj/Debug/android/src/md5a3f2b31a4f13d10f89590d80bc64f4ce/MainActivity.java
@@ -0,0 +1,47 @@
+package md5a3f2b31a4f13d10f89590d80bc64f4ce;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Xamarin_Calculator.Droid.MainActivity, Xamarin_Calculator.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Xamarin_Calculator.Droid.MainActivity, Xamarin_Calculator.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Xamarin_Calculator/Xamarin_Calculator/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Xamarin_Calculator.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+        }
+       
+        private DelegateCommand<Element> loaded;
+        public DelegateCommand<Element> Loaded =>
+            loaded ?? (loaded = new DelegateCommand<Element>(ExecuteLoaded));
+
+        void ExecuteLoaded(Element element)
+        {
+            var stackpanel = element as StackLayout;
+            var temp = 2;
+            for (int i = 3; i >= 0; i--)
+            {
+                StackLayout stack = new StackLayout()
+                {
+                    StyleId = $"Stac{i}",
+                    Orientation =StackOrientation.Horizontal
+                };
+                
+                for (int j = i*3; j > 3*temp; j--)
+                {
+                    if (j>=0)
+                    {
+                        stack.Children.Add(new Button() { Text = $"{j}", HorizontalOptions = LayoutOptions.FillAndExpand,VerticalOptions=LayoutOptions.FillAndExpand });
+                    }
+                }
+                temp--;
+                stackpanel.Children.Add(stack);
+            }
+            
+        }
+
+        void c()
+        {
+            Button button = new Button();
+        }
+    }
+}
